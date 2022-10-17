01c1fe73c21cf7644d9e62e9e59aa02bcdedf518
Jason Sturges <jason@jasonsturges.com>
2022-10-16 21:40:16 -0500

    JavaScript app example

--- apps/app-javascript/src/App.js
+++ apps/app-javascript/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { exampleJS } from "@monorepo/lib-javascript";
+import { exampleTS } from "@monorepo/lib-typescript";
 
 function App() {
   return (
@@ -9,14 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>JavaScript: {exampleJS()}</p>
+        <p>TypeScript: {exampleTS()}</p>
       </header>
     </div>
   );
