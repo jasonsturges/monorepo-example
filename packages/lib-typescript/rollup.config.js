fed21305765b9922f0afea17e6a28f73d93eb0f7
Jason Sturges <jason@jasonsturges.com>
2022-10-16 21:39:50 -0500

    TypeScript package example

---
+++ packages/lib-typescript/rollup.config.js
@@ -0,0 +1,33 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: "src/index.ts",
+    output: {
+      name: "@monorepo/lib-typescript",
+      file: pkg.browser,
+      format: "umd",
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(), //
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs", sourcemap: true },
+      { file: pkg.module, format: "es", sourcemap: true },
+    ],
+    external: [],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+  },
+];
---
+++ packages/lib-typescript/src/index.ts
@@ -0,0 +1,3 @@
+export const exampleTS = (): string => {
+  return "TypeScript Example Function";
+};
